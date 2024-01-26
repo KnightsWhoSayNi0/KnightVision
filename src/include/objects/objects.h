c1871371d407ebcdee8b74a375502b1f04a98893
John Godman <62893792+KnightsWhoSayNi0@users.noreply.github.com>
2024-01-26 03:00:37 +0000

    second project commit

---
+++ src/include/objects/objects.h
@@ -0,0 +1,6 @@
+#ifndef _OBJECTS_H_
+#define _OBJECTS_H_
+
+/* objects */
+
+#endif /* _OBJECTS_H_ */
---
+++ src/main.c
@@ -0,0 +1,23 @@
+/**
+ * Knight Vision Lighting Control Kernel
+ * v 0.1
+ * 
+ * KnightsWhoSayNi0
+*/
+
+#include <stdio.h>
+
+/* status print macros */
+#define info(str)  printf("[+] %s\n", str)
+#define warn(str)  printf("[-] %s\n", str)
+#define error(str) printf("[!] %s\n", str)
+
+int main()
+{
+    info("Starting Knight Vision Kernel");
+    info("Using DEBUG INTERFACE with current shell");
+
+    
+
+    return 1;
+}
